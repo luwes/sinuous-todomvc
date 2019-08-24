4bd2f6592b0295548c62f82292a3ab5ce2622088
Wesley Luyten <me@wesleyluyten.com>
2019-08-24 14:45:07 -0400

    Remove Sinuous map to reduce bundle size

--- dist/bundle.js
+++ dist/bundle.js
@@ -195,296 +195,6 @@
     return json;
   }
 
-  const t$1 = "__g",
-        n$1 = "nextSibling";
-
-  function e$2(r, n, e) {
-    const o = r[t$1];
-    if (o) for (r = r[n]; r && r[t$1] !== o;) r = r[n];
-    return e ? r : r[n];
-  }
-
-  function o$2(r, t, n) {
-    let e;
-
-    for (; t !== n;) e = t.nextSibling, r.removeChild(t), t = e;
-  }
-
-  function f$2(r, t, n, e) {
-    let o;
-
-    for (; t !== n;) o = t.nextSibling, r.insertBefore(t, e), t = o;
-  }
-
-  function i$2(r, t) {
-    let n = -1,
-        e = r.length;
-    if (e > 0 && r[e - 1] <= t) return e - 1;
-
-    for (; e - n > 1;) {
-      var o = (n + e) / 2 | 0;
-      r[o] > t ? e = o : n = o;
-    }
-
-    return n;
-  }
-
-  let u$2 = 0;
-  function map (l, c) {
-    const {
-      subscribe: s,
-      root: a,
-      sample: b,
-      cleanup: p
-    } = o$1,
-          m = new Map();
-    let v = document.createDocumentFragment();
-    const g = v.appendChild(document.createTextNode("")),
-          d = v.appendChild(document.createTextNode(""));
-
-    function k() {
-      m.forEach(r => r()), m.clear();
-    }
-
-    function w(r) {
-      let t = m.get(r);
-      t && t(), m.delete(r);
-    }
-
-    function S(r, n, e, o, f) {
-      return a(i => {
-        const l = function (r, n, e, o) {
-          let f;
-          const i = typeof n;
-          return "string" === i || "number" === i ? n = document.createTextNode(n) : 11 === n.nodeType && (f = n.firstChild) && f !== n.lastChild && (f[t$1] = n.lastChild[t$1] = o), r.insertBefore(n, e || null), f || n;
-        }(r, c(n, e, o), f, ++u$2);
-
-        return m.set(l, i), l;
-      });
-    }
-
-    return p(s(r => {
-      r = r || [];
-      const t = l() || [];
-      return b(() => function r(t, u, l, c, s, a, b, p) {
-        const m = l.length;
-        if (t = s.parentNode, 0 === m) return c.previousSibling || s.nextSibling ? o$2(t, c.nextSibling, s) : (t.textContent = "", t.appendChild(c), t.appendChild(s)), b && b(), [];
-
-        if (0 === u.length) {
-          for (let r = 0; r < m; r++) a(t, l[r], r, l, s);
-
-          return l.slice();
-        }
-
-        let v,
-            g,
-            d = 0,
-            k = 0,
-            w = !0,
-            S = u.length - 1,
-            y = m - 1,
-            x = c.nextSibling,
-            A = x,
-            M = s.previousSibling,
-            _ = s;
-
-        r: for (; w;) {
-          let r;
-
-          for (w = !1, v = u[d], g = l[k]; v === g;) {
-            if (d++, k++, A = x = e$2(x, n$1), S < d || y < k) break r;
-            v = u[d], g = l[k];
-          }
-
-          for (v = u[S], g = l[y]; v === g;) {
-            if (S--, y--, M = (_ = e$2(M, "previousSibling", !0)).previousSibling, S < d || y < k) break r;
-            v = u[S], g = l[y];
-          }
-
-          for (v = u[S], g = l[k]; v === g;) {
-            w = !0;
-            let n = e$2(M, "previousSibling", !0);
-            if (r = n.previousSibling, A !== n && (f$2(t, n, M.nextSibling, A), M = r), k++, --S < d || y < k) break r;
-            v = u[S], g = l[k];
-          }
-
-          for (v = u[d], g = l[y]; v === g;) {
-            if (w = !0, r = e$2(x, n$1), x !== _) {
-              let n = r.previousSibling;
-              f$2(t, x, r, _), _ = n, x = r;
-            }
-
-            if (y--, S < ++d || y < k) break r;
-            v = u[d], g = l[y];
-          }
-        }
-
-        if (y < k) {
-          if (d <= S) {
-            let r, n;
-
-            for (; d <= S;) r = (n = e$2(M, "previousSibling", !0)).previousSibling, o$2(t, n, M.nextSibling), p && p(n), M = r, S--;
-          }
-
-          return l.slice();
-        }
-
-        if (S < d) {
-          if (k <= y) for (; k <= y;) a(t, l[k], k, l, _), k++;
-          return l.slice();
-        }
-
-        const j = new Array(y + 1 - k);
-
-        for (let r = k; r <= y; r++) j[r] = -1;
-
-        const h = new Map();
-
-        for (let r = k; r <= y; r++) h.set(l[r], r);
-
-        let q = 0,
-            z = [];
-
-        for (let r = d; r <= S; r++) h.has(u[r]) ? (j[h.get(u[r])] = r, q++) : z.push(r);
-
-        if (0 === q) return r(t, r(t, u, [], c, s, a, b), l, c, s, a);
-
-        const B = function (r, t) {
-          let n = [],
-              e = [],
-              o = -1,
-              f = new Array(r.length);
-
-          for (var u = t, l = r.length; u < l; u++) {
-            var c = r[u];
-
-            if (!(c < 0)) {
-              var s = i$2(n, c);
-              -1 !== s && (f[u] = e[s]), s === o ? (n[++o] = c, e[o] = u) : c < n[s + 1] && (n[s + 1] = c, e[s + 1] = u);
-            }
-          }
-
-          for (u = e[o]; o >= 0; u = f[u], o--) n[o] = u;
-
-          return n;
-        }(j, k),
-              C = [];
-
-        let D = x;
-
-        for (let r = d; r <= S; r++) C[r] = D, D = e$2(D, n$1);
-
-        for (let r = 0; r < z.length; r++) {
-          let f = C[z[r]];
-          o$2(t, f, e$2(f, n$1)), p && p(f);
-        }
-
-        let E,
-            F = B.length - 1;
-
-        for (let r = y; r >= k; r--) B[F] === r ? (_ = C[j[B[F]]], F--) : (-1 === j[r] ? E = a(t, l[r], r, l, _) : f$2(t, E = C[j[r]], e$2(E, n$1), _), _ = E);
-
-        return l.slice();
-      }(v, r, t, g, d, S, k, w));
-    })), p(k), v;
-  }
-
-  // MIT © Luke Edwards
-  function cx(obj) {
-    var k,
-        cls = '';
-
-    for (k in obj) {
-      if (obj[k]) {
-        cls && (cls += ' ');
-        cls += k;
-      }
-    }
-
-    return cls;
-  }
-
-  const TodoApp = ctrl => {
-    return m([m("header", {
-      "class": "header"
-    }, m("h1", null, "todos"), m("input", {
-      "class": "new-todo",
-      placeholder: "What needs to be done?",
-      autofocus: true,
-      onkeyup: ctrl.addTodo
-    })), m("section", {
-      "class": "main",
-      hidden: () => ctrl.todos().length === 0
-    }, m("input", {
-      id: "toggle-all",
-      "class": "toggle-all",
-      type: "checkbox",
-      checked: () => !ctrl.remainingCount(),
-      oninput: ctrl.toggleAll
-    }), m("label", {
-      htmlFor: "toggle-all"
-    }, "Mark all as complete"), m("ul", {
-      "class": "todo-list"
-    }, map(() => ctrl.filterList(ctrl.todos()), ({
-      id,
-      title,
-      completed
-    }) => m("li", {
-      "class": () => cx({
-        completed: completed(),
-        editing: ctrl.editing() === id
-      })
-    }, m("div", {
-      "class": "view"
-    }, m("input", {
-      "class": "toggle",
-      type: "checkbox",
-      checked: completed,
-      oninput: e => ctrl.toggle(e, id)
-    }), m("label", {
-      ondblclick: () => ctrl.editing(id)
-    }, title), m("button", {
-      "class": "destroy",
-      onclick: () => ctrl.remove(id)
-    })), () => {
-      if (ctrl.editing() === id) {
-        const input = m("input", {
-          "class": "edit",
-          value: title,
-          onfocusout: e => ctrl.save(e, id),
-          onkeyup: e => ctrl.doneEditing(e, id)
-        });
-        setTimeout(() => input.focus());
-        return input;
-      }
-    })))), m("footer", {
-      "class": "footer",
-      hidden: () => ctrl.todos().length === 0
-    }, m("span", {
-      "class": "todo-count"
-    }, m("strong", null, ctrl.remainingCount), () => ctrl.remainingCount() === 1 ? ' item' : ' items', " left"), m("ul", {
-      "class": "filters"
-    }, m("li", null, m("a", {
-      "class": () => cx({
-        selected: ctrl.showMode() === 'all'
-      }),
-      href: "#/"
-    }, "All")), m("li", null, m("a", {
-      "class": () => cx({
-        selected: ctrl.showMode() === 'active'
-      }),
-      href: "#/active"
-    }, "Active")), m("li", null, m("a", {
-      "class": () => cx({
-        selected: ctrl.showMode() === 'completed'
-      }),
-      href: "#/completed"
-    }, "Completed"))), () => ctrl.completedCount() > 0 ? m("button", {
-      "class": "clear-completed",
-      onclick: ctrl.clearCompleted
-    }, "Clear completed") : null)]);
-  };
-
   const ESCAPE_KEY = 27;
   const ENTER_KEY = 13;
   function ToDosCtrl({
@@ -580,6 +290,21 @@
     };
   }
 
+  // MIT © Luke Edwards
+  function cx(obj) {
+    var k,
+        cls = '';
+
+    for (k in obj) {
+      if (obj[k]) {
+        cls && (cls += ' ');
+        cls += k;
+      }
+    }
+
+    return cls;
+  }
+
   const model = ToDosModel([]);
   const ctrl = ToDosCtrl(model);
   const stored = localStorage.getItem('todos-sinuous');
@@ -588,15 +313,94 @@
     model.todos(JSON.parse(stored).todos.map(t => ToDo(t.title, t.completed, t.id)));
   }
 
-  f(() => {
-    // store JSONized todos whenever they change
-    localStorage.setItem('todos-sinuous', JSON.stringify(model));
-  });
+  f(() => localStorage.setItem('todos-sinuous', JSON.stringify(model)));
 
-  const locationHandler = () => ctrl.showMode(location.hash.slice(2) || 'all');
+  const updateView = () => ctrl.showMode(location.hash.slice(2) || 'all');
 
-  window.addEventListener('hashchange', locationHandler);
-  locationHandler();
+  window.addEventListener('hashchange', updateView);
+  updateView();
+
+  const TodoApp = ctrl => {
+    return m([m("header", {
+      "class": "header"
+    }, m("h1", null, "todos"), m("input", {
+      "class": "new-todo",
+      placeholder: "What needs to be done?",
+      autofocus: true,
+      onkeyup: ctrl.addTodo
+    })), m("section", {
+      "class": "main",
+      hidden: () => ctrl.todos().length === 0
+    }, m("input", {
+      id: "toggle-all",
+      "class": "toggle-all",
+      type: "checkbox",
+      checked: () => !ctrl.remainingCount(),
+      oninput: ctrl.toggleAll
+    }), m("label", {
+      htmlFor: "toggle-all"
+    }, "Mark all as complete"), m("ul", {
+      "class": "todo-list"
+    }, () => ctrl.filterList(ctrl.todos()).map(({
+      id,
+      title,
+      completed
+    }) => m("li", {
+      "class": () => cx({
+        completed: completed(),
+        editing: ctrl.editing() === id
+      })
+    }, m("div", {
+      "class": "view"
+    }, m("input", {
+      "class": "toggle",
+      type: "checkbox",
+      checked: completed,
+      oninput: e => ctrl.toggle(e, id)
+    }), m("label", {
+      ondblclick: () => ctrl.editing(id)
+    }, title), m("button", {
+      "class": "destroy",
+      onclick: () => ctrl.remove(id)
+    })), () => {
+      if (ctrl.editing() === id) {
+        const input = m("input", {
+          "class": "edit",
+          value: title,
+          onfocusout: e => ctrl.save(e, id),
+          onkeyup: e => ctrl.doneEditing(e, id)
+        });
+        setTimeout(() => input.focus());
+        return input;
+      }
+    })))), m("footer", {
+      "class": "footer",
+      hidden: () => ctrl.todos().length === 0
+    }, m("span", {
+      "class": "todo-count"
+    }, m("strong", null, ctrl.remainingCount), () => ctrl.remainingCount() === 1 ? ' item' : ' items', " left"), m("ul", {
+      "class": "filters"
+    }, m("li", null, m("a", {
+      "class": () => cx({
+        selected: ctrl.showMode() === 'all'
+      }),
+      href: "#/"
+    }, "All")), m("li", null, m("a", {
+      "class": () => cx({
+        selected: ctrl.showMode() === 'active'
+      }),
+      href: "#/active"
+    }, "Active")), m("li", null, m("a", {
+      "class": () => cx({
+        selected: ctrl.showMode() === 'completed'
+      }),
+      href: "#/completed"
+    }, "Completed"))), () => ctrl.completedCount() > 0 ? m("button", {
+      "class": "clear-completed",
+      onclick: ctrl.clearCompleted
+    }, "Clear completed") : null)]);
+  };
+
   document.querySelector('.todoapp').append(TodoApp(ctrl));
 
 }());
--- src/index.js
+++ src/index.js
@@ -1,25 +1,64 @@
+import { h } from 'sinuous';
 import { S } from 'sinuous/observable';
 import { ToDosModel, ToDo } from './model.js';
-import { TodoApp } from './view.js';
 import { ToDosCtrl } from './controller.js';
+import { cx } from './utils.js';
 
 const model = ToDosModel([]);
 const ctrl = ToDosCtrl(model);
 
 const stored = localStorage.getItem('todos-sinuous');
 if (stored) {
-  model.todos(
-    JSON.parse(stored).todos.map(t => ToDo(t.title, t.completed, t.id))
-  );
+  model.todos(JSON.parse(stored).todos.map(t => ToDo(t.title, t.completed, t.id)));
 }
+S(() => localStorage.setItem('todos-sinuous', JSON.stringify(model)));
 
-S(() => {
-  // store JSONized todos whenever they change
-  localStorage.setItem('todos-sinuous', JSON.stringify(model));
-});
+const updateView = () => ctrl.showMode(location.hash.slice(2) || 'all');
+window.addEventListener('hashchange', updateView);
+updateView();
 
-const locationHandler = () => ctrl.showMode(location.hash.slice(2) || 'all');
-window.addEventListener('hashchange', locationHandler);
-locationHandler();
+const TodoApp = (ctrl) => {
+  return html`
+    <header class="header">
+      <h1>todos</h1>
+      <input class="new-todo" placeholder="What needs to be done?" autofocus onkeyup=${ctrl.addTodo} />
+    </header>
+
+    <section class="main" hidden=${() => ctrl.todos().length === 0}>
+      <input id="toggle-all" class="toggle-all" type="checkbox" checked="${() => !ctrl.remainingCount()}" oninput="${ctrl.toggleAll}" />
+      <label htmlFor="toggle-all">Mark all as complete</label>
+      <ul class="todo-list">
+        ${() => ctrl.filterList(ctrl.todos()).map(({ id, title, completed }) => html`
+          <li class=${() => cx({ completed: completed(), editing: ctrl.editing() === id })}>
+            <div class="view">
+              <input class="toggle" type="checkbox" checked="${completed}" oninput="${e => ctrl.toggle(e, id)}" />
+              <label ondblclick="${() => ctrl.editing(id)}">${title}</label>
+              <button class="destroy" onclick="${() => ctrl.remove(id)}"></button>
+            </div>
+            ${() => {
+              if (ctrl.editing() === id) {
+                const input = html`<input class="edit" value="${title}" onfocusout="${e => ctrl.save(e, id)}" onkeyup=${e => ctrl.doneEditing(e, id)} />`;
+                setTimeout(() => input.focus());
+                return input;
+              }
+            }}
+          </li>
+        `)}
+      </ul>
+    </section>
+
+    <footer class="footer" hidden=${() => ctrl.todos().length === 0}>
+      <span class="todo-count"><strong>${ctrl.remainingCount}</strong>
+        ${() => (ctrl.remainingCount() === 1 ? ' item' : ' items')} left
+      </span>
+      <ul class="filters">
+        <li><a class="${() => cx({ selected: ctrl.showMode() === 'all' })}" href="#/">All</a></li>
+        <li><a class="${() => cx({ selected: ctrl.showMode() === 'active' })}" href="#/active">Active</a></li>
+        <li><a class="${() => cx({ selected: ctrl.showMode() === 'completed' })}" href="#/completed">Completed</a></li>
+      </ul>
+      ${() => ctrl.completedCount() > 0 ? html`<button class="clear-completed" onclick=${ctrl.clearCompleted}>Clear completed</button>` : null}
+    </footer>
+  `;
+};
 
 document.querySelector('.todoapp').append(TodoApp(ctrl));
